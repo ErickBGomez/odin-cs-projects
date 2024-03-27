3ffc96aaf990aeae8ff38b0406816f81ce2103af
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-03-26 18:36:11 -0600

    Fibonacci iterative

--- fibonacci.js
+++ fibonacci.js
@@ -1,7 +1,27 @@
+function fibs(n) {
+  if (n <= 1) return n;
+
+  let total = 0;
+  const previous = [1, 0];
+
+  for (let i = 0; i < n - 1; i++) {
+    total = previous[0] + previous[1];
+    previous[1] = previous[0];
+    previous[0] = total;
+  }
+
+  return total;
+}
+
 function fibsRec(n) {
   if (n <= 1) return n;
 
   return fibsRec(n - 1) + fibsRec(n - 2);
 }
 
-console.log(fibsRec(10));
+const position = 40;
+
+console.log(`Calculating fibonacci sequence of ${position}:`);
+console.log(
+  ` - Iterative: ${fibs(position)}\n - Recursive: ${fibsRec(position)}`
+);
