5f2b7fd6dfaea17853b575a1a13df145f7c2e84e
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-05-04 22:19:50 -0600

    Insert new node into tree

--- binary-search-tree/binary-tree.js
+++ binary-search-tree/binary-tree.js
@@ -4,15 +4,15 @@ import sortAndRemoveDuplicted from "./util.js";
 class BinaryTree {
   root;
 
-  #insertTreeNode(array, start, end) {
+  #buildTreeRecursive(array, start, end) {
     if (start > end) return null;
 
     const mid = Math.trunc((start + end) / 2);
 
     const node = new Node(array[mid]);
 
-    node.left = this.#insertTreeNode(array, start, mid - 1);
-    node.right = this.#insertTreeNode(array, mid + 1, end);
+    node.left = this.#buildTreeRecursive(array, start, mid - 1);
+    node.right = this.#buildTreeRecursive(array, mid + 1, end);
 
     return node;
   }
@@ -20,7 +20,11 @@ class BinaryTree {
   buildTree(array) {
     const sortedArray = sortAndRemoveDuplicted(array);
 
-    this.root = this.#insertTreeNode(sortedArray, 0, sortedArray.length - 1);
+    this.root = this.#buildTreeRecursive(
+      sortedArray,
+      0,
+      sortedArray.length - 1
+    );
 
     return this.root;
   }
@@ -42,6 +46,25 @@ class BinaryTree {
     if (node.left !== null) {
       this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
     }
+  }
+
+  #insertRecursive(value, node) {
+    if (!node) {
+      node = new Node(value);
+      return node;
+    }
+
+    if (value < node.value) {
+      node.left = this.#insertRecursive(value, node.left);
+    } else {
+      node.right = this.#insertRecursive(value, node.right);
+    }
+
+    return node;
+  }
+
+  insert(value) {
+    this.root = this.#insertRecursive(value, this.root);
   }
 }
 
--- binary-search-tree/main.js
+++ binary-search-tree/main.js
@@ -2,7 +2,13 @@ import BinaryTree from "./binary-tree.js";
 import sortAndRemoveDuplicted from "./util.js";
 
 const tree = new BinaryTree();
+const tree2 = new BinaryTree();
 
 tree.buildTree([1, 2, 3, 4, 5, 6, 7]);
 tree.prettyPrint();
-console.log(tree.root);
+
+tree2.insert(5);
+tree2.insert(7);
+tree2.insert(2);
+
+tree2.prettyPrint();
