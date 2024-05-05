d4b0374db2fcaa5126a85e711c2b20a9b588f0cd
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-05-05 00:13:38 -0600

    Add randomArray and randomInt methods

--- binary-search-tree/util.js
+++ binary-search-tree/util.js
@@ -2,4 +2,22 @@ const sortAndRemoveDuplicted = (array) => {
   return [...new Set(array)].sort((a, b) => a - b);
 };
 
-export default sortAndRemoveDuplicted;
+// Code from StackOverflow post:
+// https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+function randomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const randomArray = (length, min, max) => {
+  const array = [];
+
+  for (let i = 0; i < length; i++) {
+    array.push(randomInt(min, max));
+  }
+
+  return array;
+};
+
+export { sortAndRemoveDuplicted, randomArray };
