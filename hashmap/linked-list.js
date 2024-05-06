1b18265c40969d1d388af21b4194782d9bf25633
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-05-05 18:17:47 -0600

    Add LinkedList class

---
+++ hashmap/linked-list.js
@@ -0,0 +1,26 @@
+import Node from "./node.js";
+
+class LinkedList {
+  #head;
+  #tail;
+
+  constructor() {
+    this.#head = null;
+    this.#tail = null;
+  }
+
+  insert(key, value) {
+    const newNode = new Node(key, value);
+
+    // Empty list
+    if (!this.#head) {
+      this.#head = newNode;
+      this.#tail = newNode;
+      return;
+    }
+
+    // Add node to end of list
+    this.#tail.next = newNode;
+    this.#tail = newNode;
+  }
+}
