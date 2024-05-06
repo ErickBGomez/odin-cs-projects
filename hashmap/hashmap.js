b92d605c33839a7800ba18297ec9b3d84d33680e
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-05-05 18:03:13 -0600

    Define private attributes to HashMap class

--- hashmap/hashmap.js
+++ hashmap/hashmap.js
@@ -1,16 +1,22 @@
 class HashMap {
-  #array = [];
+  #hashSize = 16;
+  #buckets;
+  #capacity;
+
+  constructor() {
+    this.#buckets = new Array(this.#hashSize);
+    this.#capacity = this.#buckets.length;
+  }
 
   hash(key) {
     let hashCode = 0;
-    const hashMod = 16;
 
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
       hashCode = primeNumber * hashCode + key.charCodeAt(i);
     }
 
-    return hashCode % hashMod;
+    return hashCode % this.#hashSize;
   }
 
   set(key, value) {}
--- hashmap/main.js
+++ hashmap/main.js
@@ -1,5 +1,3 @@
 import HashMap from "./hashmap.js";
 
 const hashMap = new HashMap();
-
-console.log(hashMap.hash("Yes"));
