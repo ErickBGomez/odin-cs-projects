ec795a071c2014ae1514a34143ca12a1d1aa3af3
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-07-14 12:52:46 -0600

    Set up Position clas

--- knights-travails/knightMoves.js
+++ knights-travails/knightMoves.js
@@ -1,5 +1,26 @@
-function knightMoves() {
-  console.log("knight moves");
+import Position from "./position.js";
+
+const moves = [
+  new Position(1, 2),
+  new Position(1, -2),
+  new Position(-1, 2),
+  new Position(-1, -2),
+  new Position(2, 1),
+  new Position(2, -1),
+  new Position(-2, 1),
+  new Position(-2, -1),
+];
+
+function knightMoves(start, end) {
+  const startPos = new Position(...start);
+  const endPos = new Position(...end);
+
+  const queue = [start, [start]];
+  const visited = [];
+
+  while (queue.length) {
+    const [current, path] = queue.shift();
+  }
 }
 
 export default knightMoves;
---
+++ knights-travails/position.js
@@ -0,0 +1,12 @@
+class Position {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  toArray() {
+    return [x, y];
+  }
+}
+
+export default Position;
